dee6425c413045908770cf6dfb50b9b8a6616a0d
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-05-26 19:57:57 -0600

    start of using state to do a to do list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 /* import React from 'react';
 import ReactDOM from 'react-dom'; */
 import './assets/css/Header.css';
+import './assets/css/todo.css';
 import { Header } from './Components/Header';
-import { MyLife } from './Components/MyLife';
+/* import { MyLife } from './Components/MyLife'; */
+import { TodoList } from './Components/TodoList';
 
 function App() {
   return (
     <div className = "root">
       <div className = "Header">
         {<Header name = "Thomas"/>}
+      </div>
+      <div>
+        {<TodoList />}
       </div>
     </div>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,13 +4,13 @@ import React, { useState } from 'react';
 export const Header = (props) => {
     const [display, setDisplay] = useState(true);
 
-    const myTopics = ["my Life", "my Workflow"];
+    const myTopics = ["Life", "Workflow"];
 
     return (
         <div id="header">
             <div className = "header-header">
                 <h3>Hey! My name is {props.name} and this is my React playground!</h3>
-                <h5>Welcome to, {myTopics[1]}</h5>
+                <h5>Welcome to my {myTopics[1]}</h5>
                 <button onClick={() => setDisplay(!display)}>Let's go!</button>
                 {display ? <h3>true</h3> : <h3>false</h3>}
             </div>
